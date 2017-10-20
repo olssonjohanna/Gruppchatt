a8d7e01ff990634e807d9c1948cadb5859eb0578
olssonjohanna <32932208+olssonjohanna@users.noreply.github.com>
2017-10-20 15:16:20 +0200

    Plan för projekt

--- test.py
+++ test.py
@@ -1,8 +1,54 @@
-import socket
+import tkinter as tk
+import tkinter.messagebox
 
-socket
+#USER & REGISTER FIRST PAGE
+def first_page():
 
-print("hejedj")
-import _thread
+    # REGISTER NEW SUBFUNC
+    def sub_func_register():
+        child = tkinter.Toplevel(self.root)
 
-hej
+        name = tk.Label(child, text="Name: ")
+        name_entry = tk.Entry(child)
+
+        email = tk.Label(child, text="Email: ")
+        email_entry = tk.Entry(child)
+
+        username = tk.Label(child, text="Username: ")
+        username_entry = tk.Entry(child)
+
+        password = tk.Label(child, text="Password: ")
+        password_entry = tk.Entry(child)
+
+        register_button = tk.Button(self.root, text="REGISTER", command= first_page)
+
+        name.grid()
+        name_entry.grid()
+        email.grid()
+        email_entry.grid()
+        username.grid()
+        username_entry.grid()
+        password.grid()
+        password_entry.grid()
+        login_button.grid()
+        register_button.grid()
+
+    username = tk.Label(self.root, text="Username: ")
+    username_entry = tk.Entry(self.root)
+
+    password = tk.Label(self.root, text="Password: ")
+    password_entry = tk.Entry(self.root)
+
+    login_button = tk.Button(self.root, text="LOGIN", command = XX)   #denna ska trigga chattfönstret
+
+    register_button = tk.Button(self.root, text="REGISTER NEW", command = sub_func_register)
+
+    username.grid()
+    username_entry.grid()
+    password.grid()
+    password_entry.grid()
+    login_button.grid()
+    register_button.grid()
+
+
+
