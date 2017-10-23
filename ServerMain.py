881ae597e8a2a50c4658f815c049acd426bfb3bf
fadi123456 <fadiakrawi@gmail.com>
2017-10-23 13:52:38 +0200

    Server

--- ServerMain.py
+++ ServerMain.py
@@ -1,6 +1,7 @@
 import socket
 from ServerSend import Server_sender
 from ServerRecieveAndHandler import Server_reciever_Handler
+from GUIFunctions import register
 
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
@@ -17,6 +18,7 @@ while True:
     client_socket, client_addr = server_socket.accept()
 
     list_of_client_sockets.append(client_socket)
+
 
     reciver = Server_reciever_Handler(client_socket,list_of_client_sockets,client_addr)
     reciver.start()
