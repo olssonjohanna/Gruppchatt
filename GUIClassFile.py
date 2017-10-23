65278ab90e8c74420239975d498c25ad71be92c5
Axelhagnell <32957679+Axelhagnell@users.noreply.github.com>
2017-10-23 10:02:07 +0200

    iutf

--- GUIClassFile.py
+++ GUIClassFile.py
@@ -7,16 +7,16 @@ class GuiClass:
 
     def updateChat(self,text):
         self.windowText.config(state=tk.NORMAL)
-        self.windowText.insert('1.0',text+"\n")
+        self.windowText.insert(tk.END,text+"\n")
         self.windowText.config(state=tk.DISABLED)
 
     def retrieve_input(self):
         input = self.chatText.get('1.0', tk.END)
+        self.chatText.delete('1.0', tk.END)
         self.updateChat(input)
 
-    def chatWindow(self, new_root):
+    def chatWindow(self):
         root = tk.Tk()
-        new_root.destroy()
 
         root.geometry('{}x{}'.format(600, 400))
         root.resizable(False, False)
@@ -27,9 +27,17 @@ class GuiClass:
         self.chatText = tk.Text(textFrame,height='5',width='60')
         sendBtn = tk.Button(textFrame,text='Send',height = '5',width=10,command = lambda: self.retrieve_input())
 
+        scrollb = tk.Scrollbar(chatFrame, command=self.windowText.yview)
+        scrollb.grid(row=0, column=1, sticky='nsew')
+        self.windowText['yscrollcommand'] = scrollb.set
+
+        root.bind("<Return>", lambda x: self.retrieve_input())
+
         self.windowText.config(state=tk.DISABLED)
 
-        self.windowText.pack()
+        self.windowText.grid(row=0, column=0, sticky="nsew", padx=2, pady=2)
+        chatFrame.grid_rowconfigure(0, weight=1)
+        chatFrame.grid_columnconfigure(0, weight=1)
         chatFrame.pack(fill=tk.X, padx=5, pady=20)
         textFrame.pack(fill=tk.X, padx=5, pady=20)
         self.chatText.grid(row=0)
